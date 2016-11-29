be7b60bc7454c4d4e3fb09298e76925bdbf9094f
Craig Doremus <cdoremus@gmail.com>
2016-11-29 17:16:03 -0500

    Added template tests

--- src/app/components/app/app.component.spec.ts
+++ src/app/components/app/app.component.spec.ts
@@ -9,15 +9,14 @@ import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { WindowRef } from './../../services/window/window.service';
 import { StateService } from './../../services/state/state.service';
 import { AppComponent } from './app.component';
 import { TestBed, ComponentFixture, inject } from '@angular/core/testing';
 
 
+describe('AppComponent', () => {
 
-
-describe('AppComponent2', () => {
+ describe('component logic', () => {
     let fixture: ComponentFixture<AppComponent>;
     let component: AppComponent;
 
@@ -28,100 +27,119 @@ describe('AppComponent2', () => {
                 HttpModule,
                 RouterModule.forRoot([]),
             ],
-            declarations: [AppComponent, TestComponent],
+            declarations: [ AppComponent ],
             providers: [
                 Angulartics2,
                 Angulartics2GoogleTagManager,
-                {provide: Router, useClass: MockRouter},
                 {provide: APP_BASE_HREF, useValue: '/'},
-                {provide: WindowRef, useClass: MockWindowRef},
-                StateService
-            ]
+                StateService,
+                {provide: Router, useClass: MockRouter}
+          ]
         });
-        // try {
         fixture = TestBed.createComponent(AppComponent);
         component = fixture.componentInstance;
-        // } catch (error) {
-        //   console.log('BEFORE_EACH Error:', error.message);
-        // }
     });
 
     it('should have defined AppComponent instance', () => {
-    //   try {
         fixture.detectChanges();
         expect(component).toBeDefined();
-    //   } catch (error) {
-    //       console.log('Error:', error.message);
-    //       console.log('COMPONENT:', component);
-    //   }
     });
 
-  it('should have defined router event subscription', () => {
-      try {
-    fixture.detectChanges();
-    expect(component.eventSub).toBeDefined();
-      } catch (error) {
-          console.log('Error:', error.message);
-          console.log('COMPONENT:', component);
-      }
-  });
+    it('should have defined router event subscription', () => {
+        fixture.detectChanges();
+        expect(component.eventSub).toBeDefined();
+    });
 
-  xit('should not set document.body.scrollTop for NavigationStart event',
-    inject([Router, StateService, WindowRef],
-        (router: MockRouter, stateService: StateService, windowRef: WindowRef) => {
-      try {
+    it('should unsubscribe from router events when component is destroyed', () => {
+        fixture.detectChanges();
+        spyOn(component.eventSub, 'unsubscribe');
+        fixture.destroy();
+        expect(component.eventSub.unsubscribe).toHaveBeenCalled();
+    });
 
-    //   let appComponent: AppComponent = new AppComponent(new Router(), stateService, windowRef)
-      let id = 101;
-      let navEvent: NavigationEnd = new NavigationEnd(id, 'http://code.gov', 'http://foo.com');
-      let navEventObs$: BehaviorSubject<NavigationEnd> =
-        new BehaviorSubject<NavigationEnd>(navEvent);
-    //   spyOn(router, 'events').and.returnValue(navEventObs$.asObservable());
-      // navEventObs$.next(navEvent);
-      // console.log('Router', router);
-      // router.events = navEventObs$.asObservable();
-      spyOn(router, 'events').and.returnValue(Observable.of(navEvent));
-    //   router.events = Observable.of(navEvent);
 
-      fixture.detectChanges();
+ });
 
-      expect(windowRef.nativeWindow.document.body.scrollTop).toBe(id);
-      } catch (error) {
-        console.log('should NOT set document.body.scrollTop ... Error message', error.message);
-        console.log('Error', error);
-      }
-  }));
+ describe('component template', () => {
+    let fixture: ComponentFixture<AppComponent>;
+    let component: AppComponent;
+    beforeEach(() => {
+        TestBed.resetTestingModule();
+        TestBed.configureTestingModule({
+            imports: [
+                Angulartics2Module.forRoot(),
+                HttpModule,
+                RouterModule.forRoot([]),
+            ],
+            declarations: [AppComponent],
+            providers: [
+                Angulartics2,
+                Angulartics2GoogleTagManager,
+                {provide: APP_BASE_HREF, useValue: '/'},
+                StateService,
+                {provide: Router, useClass: MockRouter}
+          ]
+        });
+
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('should have header, nav and footer elements', () => {
+        fixture.detectChanges();
+        let head = fixture.nativeElement.querySelector('header');
+        let nav = fixture.nativeElement.querySelector('nav');
+        let foot = fixture.nativeElement.querySelector('footer');
+
+        expect(head).toBeDefined();
+        expect(nav).toBeDefined();
+        expect(foot).toBeDefined();
+    });
+
+    it('should have a main element with router-outlet child', () => {
+        fixture.detectChanges();
+        let outlet = fixture.nativeElement.querySelector('main router-outlet');
+
+        expect(outlet).toBeDefined();
+    });
+
+    it('should not have additional class on root div if stateService.state.section' +
+        ' has not been set', () => {
+        let stateService = component.stateService;
+        stateService.state.section = undefined;
+
+        fixture.detectChanges();
+        let div = fixture.nativeElement.querySelector('div[class="app-container "]');
+
+        expect(div).toBeDefined();
+
+    });
+
+    it('should have additional class on root div if stateService.state.section' +
+        ' has been set', () => {
+        let section = 'foobar';
+        let stateService = component.stateService;
+        stateService.state.section = section;
+
+        fixture.detectChanges();
+        let div = fixture.nativeElement.querySelector('div[class="app-container foobar"]');
+
+        expect(div).toBeDefined();
+
+    });
+
+ });
 
 });
-
-@Component({
-    selector: 'my-foo',
-    template: `<div>Foobar</div>`
-})
-class TestComponent {
-}
-
 
 class MockRouter {
     routerState = {root: ''};
     private rootComponentType: Type<any>;
 
-    private _events: Observable<Event>;
+    private events: Observable<Event>;
 
     constructor() {
-    }
-
-    get events(): Observable<Event> {
-        if (!this._events) {
-            let id = 99;
-            let navEvent: NavigationStart = new NavigationStart(id, 'http://code.gov');
-            this._events = Observable.of(navEvent);
-        }
-        return this._events;
-    }
-
-    set events(events: Observable<Event>) {
-        this._events = events;
+        this.events = Observable.of(new NavigationStart(2, ''));
     }
 
     createUrlTree() {
@@ -134,13 +152,3 @@ class MockRouter {
 
 }
 
-class MockWindowRef extends WindowRef {
-  scrollTop: number = 99;
-  constructor() {
-    super();
-  }
-
-   get nativeWindow(): Window {
-      return <Window>{document: {body: {scrollTop: this.scrollTop}}};
-   }
-}
