0cbd45b02a24d44d921d0e14bc56b134282ea013
Craig Doremus <cdoremus@gmail.com>
2016-11-29 17:15:20 -0500

    Reverted to commit  0d6e4bf  to get rid of my previous changes

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -4,7 +4,6 @@ import { NavigationEnd, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
 import { StateService } from '../../services/state';
-import { WindowRef } from './../../services/window/window.service';
 
 @Component({
   selector: 'app',
@@ -20,19 +19,15 @@ export class AppComponent implements OnInit, OnDestroy {
     private angulartics2: Angulartics2,
     private angulartics2Gtm: Angulartics2GoogleTagManager,
     private router: Router,
-    public stateService: StateService,
-    private windowRef: WindowRef
+    public stateService: StateService
   ) {}
 
   ngOnInit() {
-    console.log('ngOnInit() called');
     this.eventSub = this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
-        console.log('Event NOT instanceof NavigationEnd', evt);
         return;
       }
-      console.log('Event instanceof NavigationEnd', evt);
-      this.windowRef.nativeWindow.document.body.scrollTop = 0;
+      document.body.scrollTop = 0;
     });
   }
 
